3d75d804e188def2fb657de346dfaac8fb1eedf8
Adrian Miller <adrianjonmiller@gmail.com>
2014-09-08 15:51:02 -0700

    Secured font import

--- wp-content/themes/starkers-child/parts/shared/html-header.php
+++ wp-content/themes/starkers-child/parts/shared/html-header.php
@@ -12,5 +12,18 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<?php wp_head(); ?>
+		<script type="text/javascript">
+		  WebFontConfig = {
+		    google: { families: [ 'Lora::latin' ] }
+		  };
+		  (function() {
+		    var wf = document.createElement('script');
+		    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		    wf.type = 'text/javascript';
+		    wf.async = 'true';
+		    var s = document.getElementsByTagName('script')[0];
+		    s.parentNode.insertBefore(wf, s);
+		  })(); </script>
 	</head>
 	<body <?php body_class(); ?>>
